96f6e04c8574e8ba65d2b6767d54e8e6771f91cb
Dom <dlazzaro2@aim.com>
2018-10-11 17:15:28 -0400

    not finished

---
+++ Lab2/Lab2/Person.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace Lab2
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Person spouse;
+
+
+        public void AskForData()
+        {
+            System.Console.Write("First Name: ");
+            this.firstName = System.Console.ReadLine();
+            System.Console.Write("Last Name: ");
+            this.lastName = System.Console.ReadLine();
+            System.Console.Write("Age: ");
+            this.age = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Spouse's First Name: ");
+            this.spouse.firstName = System.Console.ReadLine();
+            System.Console.Write("Spouse's Age: ");
+            this.spouse.age = int.Parse(System.Console.ReadLine());
+        }
+        public string GetFirstandLast()
+        {
+            return this.lastName + ", " + this.firstName + " (" + this.age + ")";
+        }
+       
+
+
+    }
+
+    
+    
+
+     
+
+
+    
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.AskForData();
+            p1.lastName = p1.spouse.lastName;
+    
+            Person p2 = new Person();
+            p2.AskForData();
+            p2.lastName = p2.spouse.lastName;
+
+            System.Console.WriteLine(p1.GetFirstandLast());
+            System.Console.WriteLine(p1.spouse.GetFirstandLast());
+            System.Console.WriteLine(p2.GetFirstandLast());
+            System.Console.WriteLine(p2.spouse.GetFirstandLast());
+
+            System.Console.ReadKey();
+
+          
+
+        }
+    }
+}
