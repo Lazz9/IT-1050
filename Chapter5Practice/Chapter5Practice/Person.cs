f1c4ccbf952ae85dd4104284edf5d74a7494f6e2
Lazz9 <domlazzaro9@gmail.com>
2018-12-04 20:30:59 -0500

    added chapter 5 practice

---
+++ Chapter5Practice/Chapter5Practice/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5Practice
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public Person(string theirFirst, string theirLast)
+        {
+            this.firstName = theirFirst;
+            this.lastName = theirLast;
+        }
+        public void PrintName()
+        {
+            System.Console.WriteLine(this.firstName + " " + this.lastName);
+        }
+
+    }
+}
---
+++ Chapter5Practice/Chapter5Practice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+     
+            Person[] people;
+            people = new Person[3];
+            people[0] = new Person("Baker", "Mayfield");
+            people[1] = new Person("Nick", "Chubb");
+            people[2] = new Person("Jarvis", "Landry");
+
+            foreach (Person person in people)
+            {
+                person.PrintName();
+            }
+
+
+            System.Console.ReadKey();
+        }
+    }
+}
